9170a6f7b6a807e935cf9727d084750b9a981667
Rohan Dongre <dongrerohan421@gmail.com>
2016-03-26 19:46:11 -0500

    Create User_Details table
    Create User_Details table with hibernate annotations.

---
+++ src/main/java/org/javabrains/maven/hibernate/FirstHibernateProject/UserDetails.java
@@ -0,0 +1,65 @@
+package org.javabrains.maven.hibernate.FirstHibernateProject;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User_Details")
+public class UserDetails {
+	private String address;
+	// @Lob // for large object.
+	private String description;
+	// @Temporal(TemporalType.DATE) // To insert only date note timestamp
+	private Date joinedDate;
+
+	@Id
+	// @Column(name = "user_id")
+	private int userId;
+
+	// @Column(name = "user_name")
+	// @Transient // to skip this field.
+	private String userName;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
